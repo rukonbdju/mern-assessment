32c7179852801f11d39bdf8d25ae6bdf8dd7eb23
rukonbd1 <rukonbd.dev@gmail.com>
2024-06-07 14:38:18 +0600

    setup using vite with tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold'>Vite + React</h1>
+      <p className="">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
